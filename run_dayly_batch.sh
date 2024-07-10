c6d98828170d2e440c111a8536564d6a626a8c13
Charles Laurioz <charleslaurioz@MacBook-Pro-de-Charles-Laurioz.local>
2024-07-10 13:51:11 +0200

    maj requirements

--- run_dayly_batch.sh
+++ run_dayly_batch.sh
@@ -2,7 +2,7 @@
 pip install -r requirements.txt
 python3 page_looper.py
 python3 single_analysis.py
-pyhton3 create_readme.py
+python3 create_readme.py
 git add . -f
 git commit -m "dayly batch"
 git push
