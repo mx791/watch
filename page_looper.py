0c6f502cb8561fcc700850904b34e27dbeb20dae
Charles Lrz <56536939+mx791@users.noreply.github.com>
2024-07-15 16:10:48 +0200

    Update page_looper.py

--- page_looper.py
+++ page_looper.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
     ]
     processes = []
     for b in brands:
-        p = Process(target=scrapp_brand, args=(b, 1000))
+        p = Process(target=scrapp_brand, args=(b, 1500))
         p.start()
         processes.append(p)
 
